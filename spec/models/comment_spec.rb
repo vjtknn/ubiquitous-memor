8ded28778732667640e4a57422fe96cb299091f2
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-03-14 21:05:01 +0100

    Add basic specs for comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#content' do
+    it { is_expected.not_to allow_value("").for(:content) }
+    it { is_expected.not_to allow_value("ab").for(:content) }
+    it { is_expected.not_to allow_value("a" * 251).for(:content) }
+  end
+
+  it { is_expected.not_to allow_value("").for(:user_id) }
+  it { is_expected.to allow_value(1).for(:user_id) }
+
+  it { is_expected.not_to allow_value("").for(:movie_id) }
+  it { is_expected.to allow_value(1).for(:movie_id) }
+
+  it { should validate_uniqueness_of(:user_id).scoped_to(:movie_id) }
+
 end
