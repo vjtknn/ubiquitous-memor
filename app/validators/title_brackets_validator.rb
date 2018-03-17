f9b798890a7516614129416d2c05b46efddb7024
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-03-17 10:58:12 +0100

    REFACTOR - create separate method responsible for validates brackets

--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -1,30 +1,35 @@
 class TitleBracketsValidator < ActiveModel::Validator
   def validate(record)
     initialize_record(record: record)
-    @brackets = { '(' => ')', '[' => ']', '{' => '}', '<' => '>' }
-    @openings = @brackets.keys
-    @closings = @brackets.values
-    @stack = []
-
-    @title.each_char.with_index do |char, i|
-      if @openings.include?(char)
-        error_message if @closings.include? @title[i + 1]
-        @stack << char
-      elsif @closings.include?(char)
-        if @stack.empty? || @brackets[@stack.pop] != char
-          error_message
-        end
-      end
-    end
-    error_message unless @stack.empty?
+    initialize_brackets_and_stack
+    validate_mismached_or_empty_brackets_in_title
   end
-
   private
 
   def initialize_record(record:)
     @record = record
     @title = record.title
   end
+
+  def initialize_brackets_and_stack
+    @brackets = { '(' => ')', '[' => ']', '{' => '}', '<' => '>' }
+    @openings = @brackets.keys
+    @closings = @brackets.values
+    @stack = []
+  end
+
+  def validate_mismached_or_empty_brackets_in_title
+    @title.each_char.with_index do |char, i|
+      if @openings.include?(char)
+        error_message if @closings.include? @title[i + 1]
+        @stack << char
+      elsif @closings.include?(char)
+        error_message if @stack.empty? || @brackets[@stack.pop] != char
+      end
+    end
+    error_message unless @stack.empty?
+  end
+
   def error_message
     @record.errors[:title] << 'mismached bracket'
   end
