15a2fa3daf21078fd67cc8a44be8f978b2b5f3c3
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-03-17 10:39:41 +0100

    REFACTOR - move error message to separate metghod

--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -1,5 +1,6 @@
 class TitleBracketsValidator < ActiveModel::Validator
   def validate(record)
+    @record = record
     @title = record.title
     @brackets = { '(' => ')', '[' => ']', '{' => '}', '<' => '>' }
     @openings = @brackets.keys
@@ -8,14 +9,20 @@ class TitleBracketsValidator < ActiveModel::Validator
 
     record.title.each_char.with_index do |char, i|
       if @openings.include?(char)
-        record.errors[:title] << 'mismached bracket' if @closings.include? @title[i+1]
+        error_message if @closings.include? @title[i + 1]
         @stack << char
       elsif @closings.include?(char)
         if @stack.empty? || @brackets[@stack.pop] != char
-          record.errors[:title] << 'mismached bracket'
+          error_message
         end
       end
     end
-    record.errors[:title] << 'mismached bracket' if !@stack.empty?
+    error_message unless @stack.empty?
+  end
+
+  private
+
+  def error_message
+    @record.errors[:title] << 'mismached bracket'
   end
 end
