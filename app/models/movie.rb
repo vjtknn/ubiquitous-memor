b57549a8e27ea8be837f5f78427ab4d9802cc0a5
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-03-17 10:26:39 +0100

    GREEN - all tests pass

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,7 +13,8 @@
 #
 
 class Movie < ApplicationRecord
+  include ActiveModel::Validations
+  validates_with TitleBracketsValidator
   belongs_to :genre
   has_many :comments
-  validates_with TitleBracketsValidator
 end
--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -1,3 +1,21 @@
 class TitleBracketsValidator < ActiveModel::Validator
+  def validate(record)
+    @title = record.title
+    @brackets = { '(' => ')', '[' => ']', '{' => '}', '<' => '>' }
+    @openings = @brackets.keys
+    @closings = @brackets.values
+    @stack = []
 
+    record.title.each_char.with_index do |char, i|
+      if @openings.include?(char)
+        record.errors[:title] << 'mismached bracket' if @closings.include? @title[i+1]
+        @stack << char
+      elsif @closings.include?(char)
+        if @stack.empty? || @brackets[@stack.pop] != char
+          record.errors[:title] << 'mismached bracket'
+        end
+      end
+    end
+    record.errors[:title] << 'mismached bracket' if !@stack.empty?
+  end
 end
